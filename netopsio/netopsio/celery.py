92d8b3fc1e9474934da816163d6cf7c4afb97c3e
James Williams <amascuba@gmail.com>
2021-12-21 11:18:09 -0600

    initial celery config

--- netopsio/netopsio/celery.py
+++ netopsio/netopsio/celery.py
@@ -7,7 +7,9 @@ from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "netopsio.settings")
 
-app = Celery("netopsio")
+app = Celery("netopsio",
+             broker=f"{os.getenv('NETOPSIO_BROKER_URL')}",
+             )
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
