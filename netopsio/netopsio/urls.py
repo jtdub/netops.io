056186fd84ceebbba70da380cc677e1d201f810d
James Williams <amascuba@gmail.com>
2021-12-24 15:32:00 -0600

    initial swagger docs

--- netopsio/netopsio/urls.py
+++ netopsio/netopsio/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-
+from django.urls import include, path, re_path
 from rest_framework.routers import DefaultRouter
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from home.views import TaskResultViewSet
 from ping.views import PingRequestViewSet, PingViewSet
 
@@ -27,9 +29,31 @@ router.register(r"ping-logs", PingRequestViewSet)
 router.register(r"ping", PingViewSet, basename="ping")
 
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="NetOps.io API",
+        default_version="v1",
+        description="NetOps.io API Documentation",
+        license=openapi.License(name="Apache License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+
 urlpatterns = [
     path("", include("home.urls"), name="home"),
     path("ping/", include("ping.urls"), name="ping"),
     path("admin/", admin.site.urls, name="admin"),
     path("api/v1/", include(router.urls), name="admin-v1"),
+    re_path(
+        r"^api/docs/(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^api/docs/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
